2d6d4606452346a13ef99411d4db384a70add627
Nox <nox.tux@gmail.com>
2025-02-23 12:20:21 -0500

    add build files

---
+++ premake5.lua
@@ -0,0 +1,20 @@
+workspace "ManiMaths"
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    configurations { "Debug" }
+    startproject "Sandbox"
+    architecture "x64"
+    language "C++"
+    cppdialect "C++20"
+    targetdir ("bin/" .. outputdir)
+    objdir ("bin-int/" .. outputdir)
+    symbols "On"
+
+    includedirs { "include/" }
+    includedirs { "ThirdParties/ManiTests/include" }
+
+project "Sandbox"
+    kind "ConsoleApp"
+    location "%{prj.name}"
+
+    files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
